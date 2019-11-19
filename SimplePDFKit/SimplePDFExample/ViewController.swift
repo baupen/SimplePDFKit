879b8dce9e0e84175ef580b28d8d3022a1cefc32
Julian Dunskus <julian.dunskus@gmail.com>
2019-11-19 22:54:52 +0100

    improved example
    the code is clearer now and looks nicer, too!

--- SimplePDFKit/SimplePDFExample/ViewController.swift
+++ SimplePDFKit/SimplePDFExample/ViewController.swift
@@ -1,16 +1,27 @@
-// Created by Julian Dunskus
-
 import UIKit
 import SimplePDFKit
 
 class ViewController: UIViewController {
+	@IBOutlet private var reasonHolder: UIView!
+	
+	private var pdfController: SimplePDFViewController! {
+		didSet { loadPDF() }
+	}
+	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-		let document = try! PDFDocument(atPath: Bundle.main.path(forResource: "example easy", ofType: "pdf")!)
-		let page = try! document.page(0)
-		
 		// embed segue
-		let pdfController = segue.destination as! SimplePDFViewController
-		pdfController.additionalSafeAreaInsets.bottom = 64
-		pdfController.page = page
+		pdfController = (segue.destination as! SimplePDFViewController)
+	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		// you can add extra insets to the pdf view like this, in case you're drawing stuff over it but want its content to be fully visible within the uncovered area
+		pdfController.additionalSafeAreaInsets.bottom = reasonHolder.frame.height
+	}
+	
+	private func loadPDF() {
+		let document = try! PDFDocument(atPath: Bundle.main.path(forResource: "example easy", ofType: "pdf")!)
+		pdfController.page = try! document.page(0)
 	}
 }
